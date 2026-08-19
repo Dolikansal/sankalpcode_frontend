c703858913a9bbc689be3e4250770b5ac4644d36
Dolikansal <kansaldolly24@gmail.com>
2026-08-19 13:20:58 +0530

    resolve file issue

--- src/components/uploadadmin.jsx
+++ src/components/uploadadmin.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import axiosClient from "../utils/axiosClient";
+import axiosClient from "../utils/axiosclient";
 function Adminuplaod() {
     const { problemid } = useParams();
     const [uploading, setuploading] = useState(false);
